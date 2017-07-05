67d6180e709f9cd86ba1cf5858ec0d3e8a6cdb45
Kamran Ali <kamiali63@gmail.com>
2017-07-05 09:22:31 +0500

    updated

---
+++ Request4Feedback/app/src/main/java/com/kisok/request4feedback/Util.java
@@ -0,0 +1,24 @@
+package com.kisok.request4feedback;
+
+/**
+ * Created by Kamran ALi on 6/20/2017.
+ */
+
+public class Util {
+    public static final String ABOUTUS = "https://www.request4feedback.com/dev/about-us.php";
+    public static final String ABOUT_US = "https://request4feedback.com/dev/about-us.php";
+    public static final String LOGIN = "https://www.request4feedback.com/dev/profile/login.php";
+    public static final String PRICING = "https://www.request4feedback.com/dev/#pricing";
+    public static final String CREATE_USER = "https://www.request4feedback.com/dev/profile/create_user.php";
+    public static final String CONTACT = "https://www.request4feedback.com/dev/contact.php";
+    public static final String FEATURES = "https://www.request4feedback.com/dev/#features";
+    public static final String HOME = "https://www.request4feedback.com/dev/#home";
+    public static final String HOME_1 = "https://request4feedback.com/dev/#";
+    public static final String HOME_2 = "https://request4feedback.com/dev/#home";
+    public static final String HOME_3 = "https://www.request4feedback.com/dev/#home";
+    public static final String FAQ = "https://request4feedback.com/dev/faq.php";
+    public static final String FAQ_1 = "https://www.request4feedback.com/dev/faq.php";
+    public static final String PRIVACY_POLICY = "https://request4feedback.com/dev/privacy-policy.php";
+    public static final String TERMS_OF_SERVICE = "https://request4feedback.com/dev/terms-of-service.php";
+    public static final String MAIL_TO = "mailto:info@request4feedback.com";
+}
