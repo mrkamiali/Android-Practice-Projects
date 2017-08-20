f376a2d15d3d8e6197858ce547a688d8915c5101
Kamran Ali <kamiali63@gmail.com>
2017-08-21 00:04:03 +0500

    update

---
+++ MyPlayGround/app/src/main/java/com/myplayground/AppController.java
@@ -0,0 +1,68 @@
+package com.myplayground;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+import com.myplayground.util.LruBitmapCache;
+
+/**
+ * Created by Kamran ALi on 8/19/2017.
+ */
+
+public class AppController extends Application {
+    public static final String TAG = AppController.class
+            .getSimpleName();
+
+    private RequestQueue mRequestQueue;
+    private static AppController mInstance;
+    private ImageLoader mImageLoader;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue,
+                    new LruBitmapCache());
+        }
+        return this.mImageLoader;
+    }
+
+}
