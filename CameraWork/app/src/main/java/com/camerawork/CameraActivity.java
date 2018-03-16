ad2fbd559ccccdfc683dfd4cf7d9f940f517829b
Kamran Ali <kamiali63@gmail.com>
2018-03-16 20:13:51 +0500

    updates

--- CameraWork/app/src/main/java/com/camerawork/CameraActivity.java
+++ CameraWork/app/src/main/java/com/camerawork/CameraActivity.java
@@ -1,13 +1,198 @@
 package com.camerawork;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CameraActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    Camera camera;
+    CircleImageView btn_take_photo;
+    CircleImageView profile_image1;
+    CircleImageView profile_image2;
+    CircleImageView profile_image3;
+    TextView cancle_tv;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    Camera.PictureCallback jpegCallback;
+    Camera.ShutterCallback shutterCallback;
+    private Bitmap bmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_camera);
+        surfaceView = findViewById(R.id.surfaceView);
+        btn_take_photo = findViewById(R.id.capture_button);
+        profile_image1 = findViewById(R.id.profile_image1);
+        profile_image2 = findViewById(R.id.profile_image2);
+        profile_image3 = findViewById(R.id.profile_image3);
+        cancle_tv = findViewById(R.id.cancle_tv);
+        surfaceHolder = surfaceView.getHolder();
+
+        //install a surface holder.callback so we get notified when underlying surface is created and destroyed
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        btn_take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraImage();
+            }
+        });
+        jpegCallback = new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+
+                FileOutputStream outputStream = null;
+                File file_image = getDir();
+                if (!file_image.exists() && !file_image.mkdirs()) {
+                    Toast.makeText(CameraActivity.this, "Cant create directory", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyymmddhhmmss");
+                String date = simpleDateFormat.format(new Date());
+                String pohtofile = "Cam_Demo" + date + ".jpg";
+                String file_name = file_image.getAbsolutePath() + "/" + pohtofile;
+                File picFile = new File(file_name);
+                try {
+                    outputStream = new FileOutputStream(picFile);
+                    outputStream.write(data);
+                    outputStream.close();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException ex) {
+
+                } finally {
+
+                }
+                Toast.makeText(CameraActivity.this, "Picture saved", Toast.LENGTH_SHORT).show();
+
+                refreshCamera();
+                refreshGallery(file_image);
+
+            }
+        };
+    }
+
+    //refresh gallery
+    void refreshGallery(File file) {
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intent.setData(Uri.fromFile(file));
+        sendBroadcast(intent);
+    }
+
+    void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            //preview surface doesnot exist
+            return;
+        }
+        //stop preview before making any changes
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+
+        }
+
+        //set preview sie and make any resize rotate or
+        //reformating the changes here
+        //start preview with new settings
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    private File getDir() {
+        File dics = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        return new File(dics, "Camera Demo");
+    }
+
+    private void cameraImage() {
+        //take picture
+        camera.takePicture(null, null, jpegCallback);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        //open camera
+        try {
+            camera = Camera.open();
+        } catch (RuntimeException e) {
+
+        }
+        Camera.Parameters parameters;
+        parameters = camera.getParameters();
+        //modify parameters
+        parameters.setPreviewFrameRate(30);
+        parameters.setPreviewSize(352, 288);
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        List<Camera.Size> sizes = parameters.getSupportedPictureSizes();
+        Camera.Size size = sizes.get(0);
+        for(int i=0;i<sizes.size();i++) {
+            if(sizes.get(i).width > size.width)
+                size = sizes.get(i);
+        }
+        parameters.setPictureSize(size.width, size.height);
+        camera.setParameters(parameters);
+        camera.setDisplayOrientation(90);
+        try {
+            //The surface has been created, now tell the camera where to draw
+            camera.setPreviewDisplay(surfaceHolder);
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        refreshCamera();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    };
+
+    private void onPictureTake(byte[] data, Camera camera) {
+
     }
 }
--- CameraWork/app/src/main/java/com/camerawork/MainActivity.java
+++ CameraWork/app/src/main/java/com/camerawork/MainActivity.java
@@ -1,32 +1,135 @@
 package com.camerawork;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
+import com.bumptech.glide.Glide;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    @InjectView(R.id.start_cam)
-    Button btn_cam;
+    private CameraView cameraView;
+    CircleImageView btn_take_photo;
+    CircleImageView profile_image1, profile_image2,
+            profile_image3, profile_image4, profile_image5;
+    TextView cancle_tv;
+    private static final String TAG = "MainActivity";
+
+    private CameraKitImage kitImage;
+    private ArrayList<CameraKitImage> imagesArray = new ArrayList<>();
+    private int count =0 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
+        btn_take_photo = findViewById(R.id.capture_button);
+        profile_image1 = findViewById(R.id.profile_image1);
+        profile_image2 = findViewById(R.id.profile_image2);
+        profile_image3 = findViewById(R.id.profile_image3);
+        profile_image4 = findViewById(R.id.profile_image4);
+        profile_image5 = findViewById(R.id.profile_image5);
 
-        btn_cam.setOnClickListener(new View.OnClickListener() {
+        cancle_tv = findViewById(R.id.cancle_tv);
+        cameraView = findViewById(R.id.camera);
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-                startActivity(intent);
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                if (imagesArray.size() > 4){
+                    imagesArray.add(4,cameraKitImage);
+
+                }else {
+                    imagesArray.add(cameraKitImage);
+                }
+                setimage(cameraKitImage);
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
             }
         });
+
+
+        btn_take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraView.captureImage();
+            }
+
+        });
+
     }
+
+    private void setimage(CameraKitImage cameraKitImage) {
+
+        if (count==5&&imagesArray.size()<5){
+            count =0;
+        }
+        if (count == 0){
+            Glide.with(MainActivity.this).load(imagesArray.get(0).getBitmap()).into(profile_image1);
+
+        } else if (count == 1 ){
+            Glide.with(MainActivity.this).load(imagesArray.get(1).getBitmap()).into(profile_image2);
+
+        } else if (count == 2 ){
+            Glide.with(MainActivity.this).load(imagesArray.get(2).getBitmap()).into(profile_image3);
+
+        } else if (count == 3 ){
+            Glide.with(MainActivity.this).load(imagesArray.get(3).getBitmap()).into(profile_image4);
+
+        } else  {
+            Glide.with(MainActivity.this).load(imagesArray.get(4).getBitmap()).into(profile_image5);
+            for (int i = 0; i < imagesArray.size(); i++) {
+                Log.d(TAG, "setimage: "+i+" val "+imagesArray.get(i).getBitmap());
+            }
+        }
+        count++;
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
+    }
+
+
 }
